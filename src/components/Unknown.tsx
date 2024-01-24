83b2c6bfd8478996e5e13a3393564a78656de8c4
Alejandro Gutiérrez <npdsolutions@gmail.com>
2024-01-24 13:42:08 +0100

    refactor(Unknown):alow space coma and slash

--- src/components/Unknown.tsx
+++ src/components/Unknown.tsx
@@ -5,21 +5,35 @@ const classError = "pl-2 !border !border-red-400 rounded bg-red-100 w-full"
 const classGood = "pl-2 !border !border-green-400 rounded bg-teal-100 w-full"
 const classNone = "pl-2 !border !border-grey-400 rounded w-full"
 
+const ERROR_CLASS = "pl-2 !border !border-red-400 rounded bg-red-100 w-full";
+const GOOD_CLASS = "pl-2 !border !border-green-400 rounded bg-teal-100 w-full";
+const NONE_CLASS = "pl-2 !border !border-grey-400 rounded w-full";
+
 const Unknown = ({text}:{text:string})=>{
     const [classString, setClassString] = useState<string>(classNone)
-    const splitedComaSpace = text.split(', ')
-    const splitedComa = text.split(',')
-    const splitedSlash = text.split('/')
-    const splitedSlashSpace = text.split(' / ')
+    const splitedComa = text.split(/, ?/);
+
+    const getClassString = (value: string) => {
+        const splitedValueComa = value.split(/, ?/);
+        const splitedValueSlash = value.split(/ ?\/ ?/);
+        console.log({value,text,splitedValueComa,splitedComa,splitedValueSlash})
+        if (value === '') {
+            return NONE_CLASS;
+        } else if (
+            value === text ||
+            splitedComa.includes(value) ||
+            splitedValueComa.every((elem:string) => splitedComa.indexOf(elem) > -1) ||
+            splitedValueSlash.every((elem:string) => splitedComa.indexOf(elem) > -1)
+        ) {
+            return GOOD_CLASS;
+        } else {
+            return ERROR_CLASS;
+        }
+    };
+    
     const handleChange = (e: React.FormEvent<HTMLInputElement>)=>{
         const value = e.currentTarget.value.toLowerCase();
-        if(value === ''){
-            setClassString(classNone)
-        } else if (value === text || splitedComaSpace.includes(value) || splitedComa.includes(value) || splitedSlash.includes(value) || splitedSlashSpace.includes(value)){
-            setClassString(classGood)
-        } else {
-            setClassString(classError)
-        } 
+        setClassString(getClassString(value));
     }
   
     return<>
