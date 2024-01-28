420b2f29a97c886574349aa16fa427026a400336
Alejandro Gutiérrez <npdsolutions@gmail.com>
2024-01-28 21:20:02 +0100

    add conffeti!

--- src/components/TableForm.tsx
+++ src/components/TableForm.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import Confetti from 'react-confetti'
 import Controls from './Controls';
 import Unknown from './Unknown';
 const TABLE_HEAD = ['infinitive', 'past', 'participle', 'español'];
@@ -44,14 +45,48 @@ function shuffleVerbs(array:Verb[]) {
 }
 
 const Row = ({infinitive, past, participle, translation}:Verb)=>{
-    const knownIndex = Math.floor(Math.random()*4)
-    const classes = "h-[3.084rem] border-grey-light border hover:bg-gray-100 p-3";
-    return <tr key={infinitive} className="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
-        <td className={classes}>{knownIndex === 0 ? infinitive : <Unknown text={infinitive}/>}</td>
-        <td className={classes}>{knownIndex === 1 ? past : <Unknown text={past}/>}</td>
-        <td className={classes}>{knownIndex === 2 ? participle: <Unknown text={participle}/>}</td>
-        <td className={classes}>{knownIndex === 3 ? translation: <Unknown text={translation}/>}</td>
-    </tr>
+  const [scores,setScores]= useState([0,0,0,0])
+  const [knownIndex, setKnownIndex]= useState(0)
+  const [isExploding, setIsExploding] = useState(false);
+  const ref = useRef<HTMLTableRowElement>(null)
+
+  useEffect(() => {
+    setKnownIndex(Math.floor(Math.random()*4))
+  }, []);
+
+  useEffect(() => {
+    const sum = scores.reduce((partialSum, a) => partialSum + a, 0) === 3
+    if(sum){
+      setIsExploding(true)
+    }
+  }, [scores]);
+
+  useEffect(() => {
+    setIsExploding(false)
+    setScores([0,0,0])
+  }, [infinitive]);
+
+  const handleChange = (index:number,score:number)=>{
+    const newScores = [...scores]
+    newScores[index]=score
+    setScores(newScores)
+  }
+
+  const poss = ref.current?.getBoundingClientRect()
+
+  const classes = "h-[3.084rem] border-grey-light border hover:bg-gray-100 p-3";
+  return <tr key={infinitive} ref={ref} className="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
+      <td className={classes}>
+      <span>{isExploding && <Confetti
+      confettiSource={{ x:poss?.x as number, y:poss?.y as number, w:poss?.width as number, h:poss?.height as number }}
+      recycle={false}
+    />}</span>
+      {knownIndex === 0 ? infinitive : <Unknown text={infinitive} onChange={(score)=>handleChange(0,score)}/>}
+      </td>
+      <td className={classes}>{knownIndex === 1 ? past : <Unknown text={past} onChange={(score)=>handleChange(1,score)}/>}</td>
+      <td className={classes}>{knownIndex === 2 ? participle: <Unknown text={participle} onChange={(score)=>handleChange(2,score)}/>}</td>
+      <td className={classes}>{knownIndex === 3 ? translation: <Unknown text={translation} onChange={(score)=>handleChange(3,score)}/>}</td>
+  </tr>
 }
 
 const TableForm = ()=>{
--- src/components/Unknown.tsx
+++ src/components/Unknown.tsx
@@ -9,14 +9,13 @@ const ERROR_CLASS = "pl-2 !border !border-red-400 rounded bg-red-100 w-full";
 const GOOD_CLASS = "pl-2 !border !border-green-400 rounded bg-teal-100 w-full";
 const NONE_CLASS = "pl-2 !border !border-grey-400 rounded w-full";
 
-const Unknown = ({text}:{text:string})=>{
+const Unknown = ({text,onChange}:{text:string,onChange:(score:number)=>void})=>{
     const [classString, setClassString] = useState<string>(classNone)
     const splitedComa = text.split(/, ?/);
 
     const getClassString = (value: string) => {
         const splitedValueComa = value.split(/, ?/);
         const splitedValueSlash = value.split(/ ?\/ ?/);
-        console.log({value,text,splitedValueComa,splitedComa,splitedValueSlash})
         if (value === '') {
             return NONE_CLASS;
         } else if (
@@ -25,8 +24,10 @@ const Unknown = ({text}:{text:string})=>{
             splitedValueComa.every((elem:string) => splitedComa.indexOf(elem) > -1) ||
             splitedValueSlash.every((elem:string) => splitedComa.indexOf(elem) > -1)
         ) {
+            onChange(1)
             return GOOD_CLASS;
         } else {
+            onChange(0)
             return ERROR_CLASS;
         }
     };
