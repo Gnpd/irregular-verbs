b9bd17af568ac46b707565596ee13fc6bdef8c06
Alejandro Gutiérrez <npdsolutions@gmail.com>
2024-10-25 13:48:24 +0200

    add github and sponsor link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,37 @@
 import TableForm from '../components/TableForm'
+import Image from 'next/image';
+
 
 export default function Home() {
   return (
     <main className="container">
       <TableForm />
-      <div className="text-center w-full">Powered by <span className="text-teal-400 font-medium hover:font-semibold"><a href="mailto:a.gutierrez@g-npd.com">Candela and Alex</a></span></div>
+      <div className="text-center w-full">
+        Powered by
+        <span className="text-teal-400">
+          <a href="mailto:a.gutierrez@g-npd.com"> Candela and Alex</a>
+        </span>
+      </div>
+
+        {/* Flex container for GitHub icon and iframe */}
+        <div className="flex items-center justify-center space-x-3 mt-5">
+          <a href="https://github.com/Gnpd/irregular-verbs">
+            <Image
+              priority
+              src="/github-mark.svg"
+              height={28}
+              width={28}
+              alt="See this project on GitHub"
+            />
+          </a>
+          <iframe 
+            src="https://github.com/sponsors/Gnpd/button"
+            title="Sponsor Gnpd" 
+            height="32" 
+            width="114" 
+          />
+        </div>
+    
     </main>
   );
 }
